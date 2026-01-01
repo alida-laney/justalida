b3e98d85ff20c1ea21ad6d3d2e24eb2933a241ee
AlidaHartwell <56845246+AlidaHartwell@users.noreply.github.com>
2025-12-31 19:08:22 -0500

    Add _site to .gitignore and remove from tracking

--- _site/dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Starting Jekyll development server..."
-echo "Site will be available at http://localhost:4000"
-echo "Press Ctrl+C to stop the server"
-echo ""
-
-bundle exec jekyll serve --drafts --livereload --host 0.0.0.0 --port 4000
